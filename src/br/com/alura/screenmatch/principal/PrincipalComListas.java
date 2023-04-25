a40c2b00ef430e5a847fc305719499fb5c384521
wagnerk78 <wagnerkroiss@gmail.com>
2023-04-25 12:24:22 -0300

    25th April

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,7 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
 
@@ -30,10 +30,22 @@ public class PrincipalComListas {
                 System.out.println("Classificação: " + filme.getClassificacao());
             }
 
-
-
-            
         }
+        List<String> buscaPorArtista = new LinkedList<>();
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Adam");
+        buscaPorArtista.add("Jacqueline");
+        buscaPorArtista.add("Wagner");
+        System.out.println(buscaPorArtista);
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);
 
+        System.out.println("Lista de Títulos ordenados.");
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
     }
 }
