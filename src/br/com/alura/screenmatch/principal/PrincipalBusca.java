8865f25afadc9798d32d7b9aa3d63772f7c578b9
wagnerk78 <wagnerkroiss@gmail.com>
2023-04-29 19:16:48 -0300

    29th April

---
+++ src/br/com/alura/screenmatch/principal/PrincipalBusca.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filem para busca: ");
+        var busca = leitura.nextLine();
+
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=2d7286ca";
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
