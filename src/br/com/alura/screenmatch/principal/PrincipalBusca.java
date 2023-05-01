daf42e15181afaa4f7df7046407e4cd495779d0e
wagnerk78 <wagnerkroiss@gmail.com>
2023-05-01 12:19:08 -0300

    01th May

--- src/br/com/alura/screenmatch/principal/PrincipalBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalBusca.java
@@ -1,5 +1,11 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,7 +16,7 @@ import java.util.Scanner;
 public class PrincipalBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filem para busca: ");
+        System.out.println("Digite um filme para busca: ");
         var busca = leitura.nextLine();
 
 
@@ -23,6 +29,16 @@ public class PrincipalBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println(meuTitulo);
+
     }
 }
